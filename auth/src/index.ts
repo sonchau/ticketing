e7cf04cb9d5fe0f4c3f66723a519ab9b4916e169
“Son <son.chau@geografia.com.au>
2020-05-04 11:53:54 +1000

    add skaffold

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,9 @@
 import express from 'express';
-import {json} from 'body-parser';
-
+import { json } from 'body-parser';
 
 const app = express();
-app.use(json())
+app.use(json());
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000')
-})
+  console.log('Listening on port 3000!!!!!!!!');
+});
